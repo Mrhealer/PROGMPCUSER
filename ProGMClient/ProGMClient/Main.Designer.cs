e513d75bf88a06b1137b1573bf55ad770d1b820b
LINH <LINH@LINH-IT>
2019-12-23 11:10:35 +0700

    update

---
+++ ProGMClient/ProGMClient/Main.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ProGMClient
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayoutStore = System.Windows.Forms.ImageLayout.Tile;
+            this.BackgroundImageStore = global::ProGMClient.Properties.Resources._1;
+            this.ClientSize = new System.Drawing.Size(796, 371);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ProGMClient/ProGMClient/Main.cs
@@ -0,0 +1,84 @@
+﻿using Management.FormState;
+using Newtonsoft.Json;
+using ProGMClient.Business;
+using ProGMClient.View.Chat;
+using ProGMClient.View.Login;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProGMClient
+{
+    public partial class Main : DevExpress.XtraEditors.XtraForm
+    {
+        FormState frmMax;
+        //private frmChat _frmChat;
+        public Main()
+        {
+            InitializeComponent();
+            frmMax = new FormState();
+            frmMax.Maximize(this);
+            SocketBussiness.tcpClient = new TcpClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SocketBussiness.tcpClient.Connect("127.0.0.1", 8888);
+
+            backgroundWorker1.RunWorkerAsync();
+            frmDangNhap frmDangNhap = new frmDangNhap(frmMax, this);
+            frmDangNhap.KeyPreview = true;
+            frmDangNhap.ShowDialog();
+        }
+
+        #region config socket
+        TcpClient clientSocket = new TcpClient();
+        #endregion
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (SocketBussiness.tcpClient.Connected)
+            {
+                try
+                {
+                    Application.DoEvents();
+                    NetworkStream ns = SocketBussiness.tcpClient.GetStream();
+                    string DuLieu = "";
+                    while (ns.DataAvailable && SocketBussiness.tcpClient.Connected)
+                    {
+                        byte[] arrByte = new byte[SocketBussiness.tcpClient.ReceiveBufferSize];
+                        ns.Read(arrByte, 0, arrByte.Length);
+                        DuLieu = Encoding.UTF8.GetString(arrByte);
+                        DuLieu = DuLieu.Substring(0, DuLieu.LastIndexOf("$")).Trim();
+                        var dataT = JsonConvert.DeserializeObject<dataSend>(DuLieu);
+                        var _frmChat = (frmChat)Application.OpenForms["frmChat"];
+                        if (_frmChat == null)
+                        {
+                            _frmChat = new frmChat();
+                        }
+                        Invoke(new Action(() =>
+                        {
+                            _frmChat.TopMost = true;
+                            _frmChat.UpdateHistory(dataT.name.ToUpper() + " Say: " + dataT.msg);
+                            _frmChat.Show();
+                        }));
+                       
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+
+        }
+    }
+}
